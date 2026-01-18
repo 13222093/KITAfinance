ab173f6aec9dbb026bcf8bdac858ca790555d9a3
Samantha Laqueenna Ginting <167011131+sammmine@users.noreply.github.com>
2026-01-18 21:24:20 +0700

    style: custom animate wiggle

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -48,6 +48,15 @@ const config: Config = {
         'active': '#F6F9F9',
         'inactive': '#7D7F9C',
       },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        },
+      },
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite',
+      },
     },
   },
   plugins: [],
